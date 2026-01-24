9e6c88ebe77576a41c30242fa5d87d1635bfd8c0
Yakolin-Net <ysantamarina@fi.uba.ar>
2026-01-24 06:02:00 -0300

    Profile  text update

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -2,17 +2,17 @@ import slugcatImg from "../assets/img/profile-slugcat.png";
 export default function Profile() {
     return (
         <div className="profile">
-            <div className="profile-specs"> 
+            <div className="profile-specs">
                 <div className="item">
-                <h1 className="subtitle" > Sobre mi </h1>
-                <img src={slugcatImg} alt="Yaco" style={{alignItems: "left", width: "80%"}}/>
+                    <h1 className="subtitle" > Sobre mi </h1>
+                    <img src={slugcatImg} alt="Yaco" style={{ alignItems: "left", width: "80%" }} />
                 </div>
-                
+
             </div>
             <br></br>
             <div className="profile-bio">
-                <p style={{textAlign: "left"}}>
-                    Soy estudiante en la Facultad de Ingenieria Universidad de Buenos Aires, estoy fuertemente interesado en el mundo del 
+                <p style={{ textAlign: "left" }}>
+                    Soy estudiante en la Facultad de Ingenieria Universidad de Buenos Aires, estoy fuertemente interesado en el mundo del
                     desarrollo de software, robotica, inteligencia artificial y desarrollo de videojuegos.
                     <br></br>
                     <br></br>
@@ -21,13 +21,11 @@ export default function Profile() {
                     <br></br>
                     Me considero una persona proactiva, con muchas ganas de aprender y enfrentar nuevos desafios en ambientes laborales o de equipo.
                     <br></br>
-                    <br></br>
-                    A continuacion les mostrare algunos de mis proyectos y mi avance en la carrera. 
                 </p>
             </div>
-            
+
         </div>
     )
 }
 
-export {Profile};
+export { Profile };
